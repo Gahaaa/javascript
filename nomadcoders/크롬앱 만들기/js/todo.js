624b0b55ef44eafbfa12f6df5929220f6c5346fc
Gahaaa <103615847+Gahaaa@users.noreply.github.com>
2022-11-30 14:28:07 +0900

    20221130 노마드 공부

--- nomadcoders/크롬앱 만들기/js/todo.js
+++ nomadcoders/크롬앱 만들기/js/todo.js
@@ -2,10 +2,39 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const toDoArr = [];
+
+function saveToDo(){
+    localStorage.setItem("todo", JSON.stringify(toDoArr));
+
+}
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintTodo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+
+    button.innerText="❌";
+    button.addEventListener("click", deleteTodo);
+
+    span.innerText= newTodo;
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
-    console.log(toDoInput.value)
+    const newTodo = toDoInput.value;
     toDoInput.value ="";
+    toDoArr.push(newTodo);
+    paintTodo(newTodo);
+    saveToDo();
 
 }
 
