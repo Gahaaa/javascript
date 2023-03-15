920ca7d0250e53a2149e33ca3997903e18379507
Gahaaa <103615847+Gahaaa@users.noreply.github.com>
2023-03-15 10:45:45 +0900

    230315 수정

--- 근거문구팝업/script.js
+++ 근거문구팝업/script.js
@@ -10,7 +10,9 @@ function layerOpen(el) {
 
 function evidenceShow(txt_pop, text_list) {
     console.log(text_list)
-  $(txt_pop).text(text_list);
+  // $(txt_pop).html(text_list);
+  // document.querySelector(txt_pop).innerHTML=text_list;   HTML 태그 요소들까지 입력(XSS 공격에 취약할 수가 있음)
+  document.querySelector(txt_pop).textContent=text_list;
 }
 
 function openTextPopup(args) {
@@ -22,6 +24,9 @@ function openTextPopup(args) {
     text02: {
       txt: "두 번째 근거문구!두 번째 근거문구!두 번째 근거문구!두 번째 근거문구!",
     },
+    text03: {
+        txt: "<p style='color:red;'>세 번째 근거문구!</p>입니다.",
+      },
   };
 
   try {
